04f6d1dd877ca6d60b42bf9ae8bd97a272b18f4c
Kacper Grela <38227679+kgrela@users.noreply.github.com>
2022-03-19 22:18:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vehicles
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
