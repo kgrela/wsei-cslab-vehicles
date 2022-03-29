89eef204a04ead17d8e0e7bc56c60742314c36ca
Kacper Grela <38227679+kgrela@users.noreply.github.com>
2022-03-29 23:46:50 +0200

    Added Amphibian class

---
+++ Vehicles/Amphibian.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vehicles.Interfaces;
+
+namespace vehicles
+{
+    public class Amphibian : EngineVehicle, IVehicle, ISailable, IDriveable
+    {
+        private readonly int w;
+        private readonly int b;
+        private Move m;
+        public Amphibian(int horsePower, int buoyancy) : base(horsePower, FuelType.Diesel)
+        {
+            availableEnv.Add(Environments.Ground);
+            availableEnv.Add(Environments.Water);
+            w = 8;
+            b = buoyancy;
+            m = new Move(true, Wheels, true, Buoyancy);
+        }
+
+        public int Buoyancy => b;
+        public int Wheels => w;
+        public string Name => GetType().Name;
+
+        public void Accelerate(double targetSpeed)
+        {
+            m.TryToAccelerate(currentEnv, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void SlowDown(double targetSpeed)
+        {
+            m.TryToSlowDown(currentEnv, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void LeaveWater()
+        {
+            m.TryToDrive(ref currentEnv, _state, ref MovingSpeed, Name);
+        }
+
+        public void Sail()
+        {
+            m.TryToSail(ref currentEnv, _state, ref MovingSpeed, Name);
+        }
+
+        public void StopVehicle()
+        {
+            m.StopMoving(ref _state, currentEnv, ref MovingSpeed, Name);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}" + base.ToString() + $"\nWheels: {Wheels}\nBuoyancy: {Buoyancy}\n";
+        }
+    }
+}
