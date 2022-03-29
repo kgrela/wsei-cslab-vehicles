d269e9f2a44adf2ccacfa67c5f48a5a81478f885
Kacper Grela <38227679+kgrela@users.noreply.github.com>
2022-03-29 23:43:13 +0200

    Added Plane class

---
+++ Vehicles/Plane.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vehicles.Interfaces;
+
+namespace vehicles
+{
+    public class Plane : EngineVehicle, IVehicle, IDriveable, IFlyable
+    {
+        private readonly int w;
+        private Move m;
+        public int Wheels => w;
+        public string Name => GetType().Name;
+        public Plane(int horsePower, FuelType fuelType) : base(horsePower, fuelType)
+        {
+            availableEnv.Add(Environments.Ground);
+            availableEnv.Add(Environments.Air);
+            w = 2;
+            m = new Move(true, Wheels, true);
+        }
+
+        public void Accelerate(double targetSpeed)
+        {
+            m.TryToAccelerate(currentEnv, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void SlowDown(double targetSpeed)
+        {
+            m.TryToSlowDown(currentEnv, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void Fly()
+        {
+            m.TryToFly(ref currentEnv, _state, ref MovingSpeed, Name);
+        }
+
+        public void Land()
+        {
+            m.TryToDrive(ref currentEnv, _state, ref MovingSpeed, Name);
+        }
+
+        public void StopVehicle()
+        {
+            m.StopMoving(ref _state, currentEnv, ref MovingSpeed, Name);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}" + base.ToString() + $"\nWheels: {Wheels}\n";
+        }
+    }
+}
